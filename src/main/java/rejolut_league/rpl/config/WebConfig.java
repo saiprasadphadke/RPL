52d22ecc3c194fe13843ae3d4784047fa95763d3
saiprasadphadke <saiprasad.p@rejolut.com>
2023-12-31 06:57:11 +0530

    Added CORS policy

---
+++ src/main/java/rejolut_league/rpl/config/WebConfig.java
@@ -0,0 +1,15 @@
+package rejolut_league.rpl.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://d484-103-190-212-216.ngrok-free.app") // Add your frontend URL here
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
